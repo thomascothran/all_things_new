9c5230e72e8a42d325a6129454742439b16c1f01
tmcothran <tmcothran@hotmail.com>
2016-09-02 15:48:54 -0400

    Added the beginnings of an inspect ability, but can't make a choice yet.

--- utils/game.py
+++ utils/game.py
@@ -13,8 +13,30 @@ class Menu():
     def __init__(self):
         self.default_prompt = {
             "l": "look around",
+            "i": "inspect an object",
             "q": "quit",
         }
+
+    def inspect(self, room, object=None, level=1):
+        """
+        This function takes an object to be inspected and prints the string.
+
+        params:
+            object: the object to be inspected
+            room: the room the player is in
+            level: the scrutiny the player is giving the object
+        """
+        if not object:
+            print("Which object would you like to inspect?\nHit b to go back.")
+            for item in room.things_in_room().keys():
+               print("{}: {}".format(item, item))
+
+        while True:
+            user_input = input(">> ")
+            if user_input.lower() == 'b':
+                break
+
+
 
     def prompt_user_and_get_user_input(self, room, prompt=None):
         """
@@ -37,8 +59,11 @@ class Menu():
         while True:
             if user_input.lower() == 'q':
                 sys.exit()
-            if user_input.lower() == 'l':
+            elif user_input.lower() == 'l':
                 print(room.inspect())
+                break
+            elif user_input.lower() == 'i':
+                self.inspect(room)
                 break
         return True
 
